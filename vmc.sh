9f5c5f4eb4bd19196cf963881e296dffefd7dac0
jonaproitz <jonaproitz@gmail.com>
2024-05-14 12:59:55 +0200

    add more functionality

--- vmc.sh
+++ vmc.sh
@@ -1,2 +1,24 @@
+# Keywords: vmc, grid, int, boot, plot
 
-python3 src/simulation_scripts/"$1"
+
+if [ -z "$1" ]
+then
+    python3 src/simulation_scripts/vmc_playground.py
+elif [ "$1" == vmc ]
+then
+    python3 src/simulation_scripts/vmc_playground.py
+elif [ "$1" == grid ]
+then
+    python3 src/simulation_scripts/grid_search.py
+elif [ "$1" == int ]
+then
+    python3 src/simulation_scripts/interaction_vs_particles.py
+elif [ "$1" == boot ]
+then
+    python3 src/simulation_scripts/bootstrap.py
+elif [ "$1" == plot ]
+then 
+    python3 src/simulation_scripts/plot_producer.py
+else
+    python3 src/simulation_scripts/"$1"
+fi
